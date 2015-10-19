edd399667a70bf6b053c44fa69c23fd08effe9e9
mammut <jtuki@foxmail.com>
2015-10-19 22:48:06 +0800

    python huge mapping find key. 结论：基本上大部分时间都浪费在 random 上。1000000 个元素的 mapping 随机查找还是挺快的。 小新 v1000 - 4.530440549502606 seconds (10 times) - excluding create_mapping().

---
+++ src/mapping.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+# jtuki@foxmail.com
+
+import random
+
+gl_bytes_mapping = dict()
+gl_bytes_list = list()
+
+def generate_multiple_bytes(n):
+    assert n > 0
+    # local cache
+    generate_multiple_bytes.cached_bytes = set()
+    
+    while True:
+        bytes_seq = bytes([random.randint(0, 255) for i in range(n)])
+        if bytes_seq not in generate_multiple_bytes.cached_bytes:
+            generate_multiple_bytes.cached_bytes.add(bytes_seq)
+            break
+        
+    return bytes_seq
+    
+def get_mapping_value(bytes_seq):
+    assert bytes_seq in gl_bytes_mapping
+    return gl_bytes_mapping[bytes_seq]
+    
+def test_get_mapping():
+    i = 0
+    for b in gl_bytes_list:
+        v = gl_bytes_mapping[b]
+        i += 1
+    return i
+    
+def create_mapping(n):
+    global gl_bytes_list
+    global gl_bytes_mapping
+    
+    gl_bytes_list = list()
+    gl_bytes_mapping = dict()
+ 
+    for i in range(n):
+        b = generate_multiple_bytes(12)
+        gl_bytes_list.append(b)
+        gl_bytes_mapping[b] = b*5
+    
+if __name__ == '__main__':
+    import timeit
+    create_mapping(1000000)
+    gl_bytes_list.sort()
+    print (timeit.timeit('assert 1000000 == test_get_mapping()', 'from __main__ import test_get_mapping', number=10))
